63692ac6ebe3bb7dfcea46755008d9ec6cb884bd
ollehhh <oleh.zarichnyi@gmail.com>
2019-09-09 02:06:23 +0300

    Adding class for task243a

---
+++ Chapter7/TwoHundredFortyThreeA.java
@@ -0,0 +1,20 @@
+package Chapter7;
+
+import java.util.ArrayList;
+
+public class TwoHundredFortyThreeA {
+    public ArrayList<Integer> isRepresentable( Integer n) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for (int i=1; i<=n; i++){
+            for(int j=1; j<=n;j++){
+                if((i*i+j*j)==n){
+                    a.add(i);
+                    a.add(j);
+                }
+            }
+        }
+
+       return a;
+    }
+
+}
