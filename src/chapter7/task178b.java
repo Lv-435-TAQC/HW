29d551ffd8e5ca8b21a61d1f48e395844f059e92
YuraSmoliy <yura140690@gmail.com>
2019-09-09 17:03:12 +0300

    fix

--- src/chapter7/task178b.java
+++ src/chapter7/task178b.java
@@ -1,13 +1,10 @@
 package chapter7;
-import creationExaptions.taskExeption;
 
 public class task178b {
-    public int multiplesOfThreeAndNotMultiplesOfFive (int[] array) throws taskExeption{
+    public static int multiplesOfThreeAndNotMultiplesOfFive (int[] array) {
         int count = 0;
-        if(array.length == 0)throw new taskExeption("your array is empty");
         for(int item : array){
             if((item % 3 == 0) && (item % 5 != 0)){
-                System.out.println(item);
                 count ++;
             }
         }
