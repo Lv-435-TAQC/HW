b8ec7a50569cc9edf267e195b32aec52eb154260
ollehhh <oleh.zarichnyi@gmail.com>
2019-09-10 00:50:36 +0300

    Format code

--- Chapter7/OneHundredSeventyEightH.java
+++ Chapter7/OneHundredSeventyEightH.java
@@ -3,17 +3,13 @@ package Chapter7;
 import java.util.ArrayList;
 
 public class OneHundredSeventyEightH {
-    public int calculate(ArrayList<Integer> a){
-        for (int i=0;i<a.size();i++){
-
-             if(!(Math.sqrt(a.get(i))%2==0) || a.get(i)==0 ){
-
+    public int calculate(ArrayList<Integer> a) {
+        for (int i = 0; i < a.size(); i++) {
+            if (!(Math.sqrt(a.get(i)) % 2 == 0) || a.get(i) == 0) {
                 a.remove(i);
                 i--;
             }
-
-
         }
-       return a.size();
+        return a.size();
     }
 }
