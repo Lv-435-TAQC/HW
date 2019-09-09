cb9dfe7e79f9f26f9e9108f4ed997ddadd75e72a
YuraSmoliy <yura140690@gmail.com>
2019-09-09 18:09:51 +0300

    fix2

--- src/chapter7_2/task226.java
+++ src/chapter7_2/task226.java
@@ -5,7 +5,7 @@ public class task226 {
     public static LinkedList<Integer> commonMultipleOfTwoNumbers(int m, int n){
         LinkedList<Integer>listMultipleNumbers = new LinkedList<>();
         int max = m * n;
-        for(int iter = max; iter > 0; iter--){
+        for(int iter = max - 1; iter > 0; iter--){
             if((iter % m) == 0  && (iter % n) == 0) {
                 listMultipleNumbers.add(iter);
             }
