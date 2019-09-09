62c8d94f288880eb45f79e3861d679ac19546d6e
YuraSmoliy <yura140690@gmail.com>
2019-09-09 17:58:41 +0300

    fix2

--- src/chapter10/task329.java
+++ src/chapter10/task329.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class task329 {
     public static LinkedList<Integer> squareSumOfDigitsWhichEqualToGivenNumber(int n, int m){
             LinkedList<Integer> listEqualseSquareSum = new LinkedList<>();
-            for( int i = n; i > 0 ; i-- ){
+            for( int i = n - 1; i > 0 ; i-- ){
                 int sumDigitsOfNumber=0;
                 for(int j = i ; j > 0;) {
                     sumDigitsOfNumber += (j % 10);
