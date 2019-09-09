08e539361a56a85e84ca6c09ba262d16372afd0f
ollehhh <oleh.zarichnyi@gmail.com>
2019-09-10 00:47:28 +0300

    Format code

--- Chapter10/ThreeHundredThirtyOneA.java
+++ Chapter10/ThreeHundredThirtyOneA.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class ThreeHundredThirtyOneA {
     public ArrayList<Integer> isRepresentable(Integer n) {
         ArrayList<Integer> a = new ArrayList<Integer>();
-        for (int i=1; i<=n; i++){
-            for(int j=1; j<=n;j++){
-                for(int h=1; h<=n;h++) {
-                    if ((i * i + j * j + h*h) == n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int h = 1; h <= n; h++) {
+                    if ((i * i + j * j + h * h) == n) {
                         a.add(i);
                         a.add(j);
                         a.add(h);
@@ -16,7 +16,6 @@ public class ThreeHundredThirtyOneA {
                 }
             }
         }
-
         return a;
     }
 }
