0bb88397ebe8db34e09ac0721325ae2990a0cd7b
YuraSmoliy <yura140690@gmail.com>
2019-09-09 17:13:23 +0300

    fix

--- src/chapter7_2/task226.java
+++ src/chapter7_2/task226.java
@@ -1,11 +1,9 @@
 package chapter7_2;
-import creationExaptions.taskExeption;
 import java.util.LinkedList;
 
 public class task226 {
-    public LinkedList<Integer> commonMultipleOfTwoNumbers(int m, int n) throws taskExeption{
+    public static LinkedList<Integer> commonMultipleOfTwoNumbers(int m, int n){
         LinkedList<Integer>listMultipleNumbers = new LinkedList<>();
-        if(m < 1 || n < 1 )  throw new taskExeption("don`t use digitals less then 1");
         int max = m * n;
         for(int iter = max; iter > 0; iter--){
             if((iter % m) == 0  && (iter % n) == 0) {
