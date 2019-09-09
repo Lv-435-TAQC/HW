a11c6a529a6dd30739c4332865aacc21481c71c3
YuraSmoliy <yura140690@gmail.com>
2019-09-09 17:30:08 +0300

    fix

--- src/chapter10/task329.java
+++ src/chapter10/task329.java
@@ -1,13 +1,11 @@
 package chapter10;
-import creationExaptions.taskExeption;
+
 import java.util.LinkedList;
 
 public class task329 {
-    public LinkedList<Integer> squareSumOfDigitsWhichEqualToGivenNumber(int n, int m)throws taskExeption{
+    public static LinkedList<Integer> squareSumOfDigitsWhichEqualToGivenNumber(int n, int m){
             LinkedList<Integer> listEqualseSquareSum = new LinkedList<>();
-        if(m < 1 || n < 1)
-            throw new taskExeption("check your digitals, don`t use arguments less then 1");
-            for( int i = n - 1 ; i > 0 ; i-- ){
+            for( int i = n; i > 0 ; i-- ){
                 int sumDigitsOfNumber=0;
                 for(int j = i ; j > 0;) {
                     sumDigitsOfNumber += (j % 10);
