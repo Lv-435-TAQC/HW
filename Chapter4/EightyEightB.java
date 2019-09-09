327da215aa0fa556f98a9fee0aca084be531f8d4
ollehhh <oleh.zarichnyi@gmail.com>
2019-09-10 00:52:31 +0300

    Format code

--- Chapter4/EightyEightB.java
+++ Chapter4/EightyEightB.java
@@ -1,11 +1,8 @@
 package Chapter4;
 
 public class EightyEightB {
-   public int reorder(Integer n){
-        String change =new StringBuffer(Integer.toString(n)).reverse().toString();
-
-        return n=Integer.parseInt(change);
+    public int reorder(Integer n) {
+        String change = new StringBuffer(Integer.toString(n)).reverse().toString();
+        return n = Integer.parseInt(change);
     }
-
-
 }
