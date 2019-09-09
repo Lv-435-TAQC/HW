8b966a005e72dec614e55c7b5ebacc71eb99411c
ollehhh <oleh.zarichnyi@gmail.com>
2019-09-10 01:36:46 +0300

    Update code and add Main, Run classes
    Add static to method of task.
    Add Main class with calling menu function in Run
    Add Run class with menu and calling task

--- Chapter10/ThreeHundredThirtyOneA.java
+++ Chapter10/ThreeHundredThirtyOneA.java
@@ -3,7 +3,7 @@ package Chapter10;
 import java.util.ArrayList;
 
 public class ThreeHundredThirtyOneA {
-    public ArrayList<Integer> isRepresentable(Integer n) {
+    public static ArrayList<Integer> isRepresentable(Integer n) {
         ArrayList<Integer> a = new ArrayList<Integer>();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
