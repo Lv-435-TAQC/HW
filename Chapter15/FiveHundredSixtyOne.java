0f23416eacbd9be40b2f6f7d3c55bf9992fb9162
ollehhh <oleh.zarichnyi@gmail.com>
2019-09-10 00:38:09 +0300

    Optimize code, format code

--- Chapter15/FiveHundredSixtyOne.java
+++ Chapter15/FiveHundredSixtyOne.java
@@ -3,15 +3,13 @@ package Chapter15;
 import java.util.ArrayList;
 
 public class FiveHundredSixtyOne {
-    public ArrayList<Integer> find(Integer n){
+    public ArrayList<Integer> find(Integer n) {
         ArrayList<Integer> a = new ArrayList<>();
-        String one,two;
-        for (int i=1;i<=n;i++){
-            one = Integer.toString(i);
-            two = Integer.toString(i*i);
-            two = two.substring(two.length()-one.length());
-            if(Integer.parseInt(one)==Integer.parseInt(two)){
-
+        String test;
+        for (int i = 1; i <= n; i++) {
+            test = Integer.toString(i * i);
+            test = test.substring(test.length() - Integer.toString(i).length());
+            if (i == Integer.parseInt(test)) {
                 a.add(i);
             }
         }
