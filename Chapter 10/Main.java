c42572b7b36ad4e0296f25f5efb99e312969584f
lyubomyrr <46572442+lyubomyrr@users.noreply.github.com>
2019-09-10 01:50:21 +0300

    Add files via upload

---
+++ Chapter 10/Main.java
@@ -0,0 +1,10 @@
+package Task4;
+
+import Task4.Task331b;
+
+public class Main {
+    public static void main(String[] args) {
+    Runner runner = new Runner();
+    runner.run();
+    }
+}
---
+++ Chapter 10/Runner.java
@@ -0,0 +1,7 @@
+package Task4;
+
+public class Runner {
+    public void run(){
+        Task331b.findNums(1000);
+    }
+}
