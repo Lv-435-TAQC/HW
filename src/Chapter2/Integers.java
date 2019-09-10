92d11a89d1db82fa14d34a31bd06fa80413d796f
YuraSmoliy <yura140690@gmail.com>
2019-09-10 15:07:27 +0300

    rename

--- src/Chapter2/Integers.java
+++ src/Chapter2/Integers.java
@@ -3,7 +3,7 @@ package Chapter2;
 import java.util.LinkedList;
 
 public class Integers {
-    public static LinkedList<Integer> task559(int n){
+    public static LinkedList<Integer>  task559(int n){
         LinkedList<Integer> numbers = new LinkedList<>();
         int mersensMumber = 0;
         for(int i = 0 ; i < n ; i++ ){
