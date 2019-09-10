2e98d2f990f796f60534d972933b42608e5c7338
ollehhh <oleh.zarichnyi@gmail.com>
2019-09-10 13:11:11 +0300

    Refactor code

--- src/Chapter1/CyclesAndBranching.java
+++ src/Chapter1/CyclesAndBranching.java
@@ -1,9 +1,9 @@
-package Chapter7;
+package Chapter1;
 
 import java.util.ArrayList;
 
-public class OneHundredSeventyEightH {
-    public static int calculate(ArrayList<Integer> a) {
+public class CyclesAndBranching {
+    public static int task178h(ArrayList<Integer> a) {
         for (int i = 1; i < a.size() - 1; i++) {
             if (a.get(i) < (a.get(i - 1) + a.get(i + 1)) / 2) {
                 a.remove(i);
