efa361aa44e1ed141bd40ab9c90cda56059936fa
YuraSmoliy <yura140690@gmail.com>
2019-09-10 14:30:51 +0300

    rename

---
+++ src/Chapter2/Integers.java
@@ -0,0 +1,17 @@
+package Chapter2;
+
+import java.util.LinkedList;
+
+public class Integers {
+    public static LinkedList<Integer> task559(int n){
+        LinkedList<Integer> numbers = new LinkedList<>();
+        int mersensMumber = 0;
+        for(int i = 0 ; i < n ; i++ ){
+            mersensMumber = ((int)Math.pow( 2, i) - 1);
+            if(mersensMumber < n){
+                numbers.add(mersensMumber);
+            }
+        }
+        return numbers;
+    }
+}
