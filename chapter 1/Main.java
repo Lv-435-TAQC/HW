2de64cead56d59339bf93c712a3e5ccff9b92607
Liubomyr Karp <lyubomur.karp@gmail.com>
2019-09-12 01:35:53 +0300

    Create Main.java

---
+++ chapter 1/Main.java
@@ -0,0 +1,8 @@
+import chapter1.Task243b.CyclesAndBranching ;
+
+public class Main {
+    public static void main(String[]args) {
+        Task243b ttt = new Task243b();
+        ttt.getNum(100);
+    }
+}
