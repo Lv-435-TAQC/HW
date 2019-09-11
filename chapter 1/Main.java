cc3becfc822fdf677a1044199a1b655eb16ddd06
Liubomyr Karp <lyubomur.karp@gmail.com>
2019-09-12 01:46:51 +0300

    Update Main.java

--- chapter 1/Main.java
+++ chapter 1/Main.java
@@ -1,8 +1,8 @@
-import chapter1.Task243b.CyclesAndBranching ;
+import chapter1.CyclesAndBranching ;
 
 public class Main {
     public static void main(String[]args) {
-        Task243b ttt = new Task243b();
-        ttt.getNum(100);
+        CyclesAndBranching ttt = new CyclesAndBranching ();
+        ttt.Task243b(100);
     }
 }
