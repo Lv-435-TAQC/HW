afff8a71eb54b5fa459afcb818ce2774db2788de
Liubomyr Karp <lyubomur.karp@gmail.com>
2019-09-12 01:52:22 +0300

    Create Main.java

---
+++ Chapter 1/Main.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+import chapter1.NestedLoops ;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+    Runner runner = new Runner();
+    runner.run();
+    }
+}
