1095f7ba9a4fa32dcbe6214b28155878fbbd4433
Orest Zhmurkevych <orest.zhmurkevych@gmail.com>
2019-09-15 02:33:21 +0300

    Created a test for task86

--- src/tests/chapter1/SimpleLoopsTest.java
+++ src/tests/chapter1/SimpleLoopsTest.java
@@ -3,7 +3,7 @@ package tests.chapter1;
 import chapter1.SimpleLoops;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SimpleLoopsTest {
 
@@ -11,7 +11,7 @@ class SimpleLoopsTest {
     void task86a() {
         int expected = 3;
         int actual = SimpleLoops.task86a(222);
-       assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -26,5 +26,12 @@ class SimpleLoopsTest {
         int expected = 0;
         int actual = SimpleLoops.task107(2);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void task86btest() {
+        int expectedResult = 5;
+        int actualResult = SimpleLoops.task86b(32);
+        assertEquals(expectedResult, actualResult);
     }
 }
