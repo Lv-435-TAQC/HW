f8720c3996afa4f338adc84943b745181f447bc1
Orest Zhmurkevych <orest.zhmurkevych@gmail.com>
2019-09-25 21:12:36 +0300

    Refactored code and changed reading methods in some RunTasks methods

--- src/utils/ConsoleReader.java
+++ src/utils/ConsoleReader.java
@@ -2,47 +2,19 @@ package utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ConsoleReader {
     private static Scanner in = new Scanner(System.in);
     private static BufferedReader bufferedReader = null;
-    public ConsoleReader(BufferedReader br){
+
+    public ConsoleReader(BufferedReader br) {
         bufferedReader = br;
     }
+
     static String readString() {
         return in.nextLine();
-    }
-
-    public static String readPositiveInteger() {
-        final InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-        final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String result = "";
-        do {
-            System.out.println("Enter the correct number");
-            try {
-                result = bufferedReader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } while (result.equals("") || result.substring(0, 1).equals("-") || !Util.isNumeric(result));
-        return result;
-    }
-
-    public  int readInteger() {
-        String result = "";
-        do {
-            System.out.println("Enter integer");
-            try {
-
-                result = bufferedReader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } while (!isInteger(result));
-        return Integer.parseInt(result);
     }
 
     public static boolean isInteger(String string) {
@@ -67,7 +39,7 @@ public class ConsoleReader {
             System.out.println("Enter integer ('/' char - end of numbers)");
             try {
                 result = bufferedReader.readLine();
-                if(result.contentEquals("/"))
+                if (result.contentEquals("/"))
                     continue;
                 if (isInteger(result)) {
                     array.add(Integer.valueOf(result));
@@ -77,5 +49,19 @@ public class ConsoleReader {
             }
         } while (!result.contentEquals("/"));
         return array;
+    }
+
+    public int readInteger() {
+        String result = "";
+        do {
+            System.out.println("Enter integer");
+            try {
+
+                result = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } while (!isInteger(result));
+        return Integer.parseInt(result);
     }
 }
